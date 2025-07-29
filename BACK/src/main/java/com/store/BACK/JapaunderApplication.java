e51e9cd7d7f3f19a5d4e8c21ff76299997680225
viniciusdev00 <vinicius.biancolini.tds24@gmail.com>
2025-07-29 17:21:04 -0300

    inicio do back

---
+++ BACK/src/main/java/com/store/BACK/JapaunderApplication.java
@@ -0,0 +1,13 @@
+package com.store.BACK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaunderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaunderApplication.class, args);
+	}
+
+}
