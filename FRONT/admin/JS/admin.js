99175f6f51c9c45f068511e18ed6e09c75565507
Vinicius Biancolini <vinicius.biancolini.tds24@gmail.com>
2025-09-11 08:38:46 -0300

    Update admin.js

--- FRONT/admin/JS/admin.js
+++ FRONT/admin/JS/admin.js
@@ -31,12 +31,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- Configuração do Cliente API ---
     const apiClient = axios.create({
-        baseURL: 'http://localhost:8080/api/admin',
+        baseURL: 'https://minhaloja-produção-1705.up.railway.app/api/admin',
         headers: { 
-            'Authorization': `Bearer ${token}`,
+        'Authorization': `Bearer ${token}`,
         }
     });
-    const publicApiClient = axios.create({ baseURL: 'http://localhost:8080/api' });
+    const publicApiClient = axios.create({ baseURL: 'https://minhaloja-produção-1705.up.railway.app/api' });
 
     // --- Elementos do DOM ---
     const pedidosSection = document.getElementById('pedidos-section');
