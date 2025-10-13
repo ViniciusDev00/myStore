893b20b5e54e4db6a73f8e136ffb69d684b62a86
Vinicius Biancolini <vinicius.biancolini.tds24@gmail.com>
2025-10-13 10:45:18 -0300

    Update UsuarioDTO.java

--- BACK/src/main/java/com/store/BACK/dto/UsuarioDTO.java
+++ BACK/src/main/java/com/store/BACK/dto/UsuarioDTO.java
@@ -1,7 +1,11 @@
 package com.store.BACK.dto;
 
+import com.store.BACK.model.Endereco;
+import com.store.BACK.model.Pedido;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,4 +14,9 @@ public class UsuarioDTO {
     private String nome;
     private String email;
     private String role;
+
+    // --- CORREÇÃO APLICADA AQUI ---
+    // Adicionamos os campos que faltavam para endereços e pedidos.
+    private List<Endereco> enderecos;
+    private List<Pedido> pedidos;
 }
