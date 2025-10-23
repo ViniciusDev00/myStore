6ce1890508051e0f2c84ee223cd34c6215886223
viniciusdev00 <vinicius.biancolini.ts24@gmail.com>
2025-10-22 22:48:04 -0300

    semfr

---
+++ BACK/src/main/java/com/store/BACK/controller/UsuarioController.java
@@ -0,0 +1,39 @@
+package com.store.BACK.controller;
+
+import com.store.BACK.dto.UsuarioDTO;
+import com.store.BACK.model.Pedido;
+import com.store.BACK.model.Usuario;
+import com.store.BACK.service.PedidoService;
+import com.store.BACK.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuario")
+public class UsuarioController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping("/meus-dados")
+    public ResponseEntity<UsuarioDTO> getMeusDados(@AuthenticationPrincipal Usuario usuarioLogado) {
+        // O UsuarioService é responsável por buscar os dados e montar o DTO
+        UsuarioDTO usuarioDTO = usuarioService.getDadosUsuario(usuarioLogado); 
+        return ResponseEntity.ok(usuarioDTO);
+    }
+
+    @GetMapping("/meus-pedidos")
+    public ResponseEntity<List<Pedido>> getMeusPedidos(@AuthenticationPrincipal Usuario usuarioLogado) {
+        List<Pedido> pedidos = pedidoService.getPedidosByUsuarioId(usuarioLogado.getId());
+        return ResponseEntity.ok(pedidos);
+    }
+}
