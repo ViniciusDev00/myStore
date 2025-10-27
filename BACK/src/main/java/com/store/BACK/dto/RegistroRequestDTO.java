6b0e44d76f7c50df3656ade75f64ec719eee24eb
MykyBalb <mykaelbalbino06@gmail.com>
2025-10-27 17:25:53 -0300

    nada

--- BACK/src/main/java/com/store/BACK/dto/RegistroRequestDTO.java
+++
@@ -1,5 +0,0 @@
-package com.store.BACK.dto;
-
-// Este DTO define que para o registro, apenas nome, email e senha são necessários.
-public record RegistroRequestDTO(String nome, String email, String senha) {
-}
